327d74433e618546782a3ffa67ad023d8257d65e
Anna Margotta <anna.margotta@mymail.champlain.edu>
2020-04-08 14:53:31 -0400

    Committing Anna's prototype.

---
+++ MikasIsland_Prototype2D_1/Assets/Scripts/SpawnCoco.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SpawnCoco : MonoBehaviour
+{
+    public Transform prefab;
+    public int cocoNum = 1;
+    public TextMeshProUGUI cocoNumText;
+    
+    public void SpawnCoconut()
+    {
+        Instantiate(prefab, new Vector2(-2, 0), Quaternion.identity);
+        cocoNum++;
+    }
+
+    private void Update()
+    {
+        cocoNumText.text = cocoNum.ToString();
+    }
+}
