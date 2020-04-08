d20ce2b5979a1ae6f60c05a707c1fe333cdd17f4
Petra Jarrett <petra.jarrett@mymail.champlain.edu>
2020-04-08 14:30:07 -0400

    added 3d prototype to repo

---
+++ Mika's Island Prototype/Assets/Scripts/TreeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour
+{
+    public GameObject coconutPrefab;
+    public float distance;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();   
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Vector3.Distance(gameObject.transform.position, player.transform.position) < distance)
+        {
+            Instantiate(coconutPrefab, player.transform.position + new Vector3(0, 3f, 0), Quaternion.identity);
+        }
+    }
+}
